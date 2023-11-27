d44c3154fb380fc6e7d4a81d7e0c929e8bd8a587
Yme <yghuang@mails.ccnu.edu.cn>
2023-11-27 16:39:06 +0100

    v4.1 make histogram

---
+++ src1.cxx
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <fstream>
+
+#include "TFile.h"
+#include "TChain.h"
+#include "TH2F.h"
+#include "TH1F.h"
+#include "TString.h"
+
+#include "tree_reader.h"
+#include "./CentCorrTool/CentParams.h"
+
+
+int main(){
+    /*
+    
+        This src1 is for:
+            - check the raw RefMult3 distributions
+    
+    
+    */
+    using std::cout;
+    using std::string;
+    using std::vector;
+    using std::ifstream;
+    vector<string> file_list;
+    ifstream fin("file.list");
+    string strtmp;
+    int n_file = 0;
+    while (std::getline(fin, strtmp)){
+        n_file ++;
+        if (n_file % 500 == 0){
+            cout << "[LOG] " << n_file << " files found.\n";
+        }
+        file_list.push_back(strtmp);
+    }
+    cout << "[LOG] All " << n_file << " files found.\n";
+
+    TChain* chain = new TChain("centTree");
+
+    Int_t ifile = 0;
+    for (auto iter=file_list.begin(); iter!=file_list.end(); iter++){
+        ifile ++;
+        if (ifile % 500 == 0){
+            cout << "[LOG] " << ifile << " trees read.\n";
+        }
+        chain->Add(iter->c_str());
+    }
+    cout << "[LOG] All " << ifile << " trees read.\n";
+
+    cout << "[LOG] Preparing to get histograms.\n";
+    TreeReader* maker = new TreeReader();
+    maker->init_reader(chain);
+    Int_t nev = maker->get_entries();
+
+    CentCorrTool* corr = maker->GetCentCorrTool();
+    corr->SetDoMatchPileUp(true);
+    corr->SetDoBetaPileUp(true);
+    corr->SetDoLumi(false);
+    corr->SetDoVz(false);
+    corr->ReadParams();
+
+
+    const int nTrg = cent_conf::nTrg;
+
+    TH2F* hVzRef3[nTrg];
+    for (int i=0; i<nTrg; i++) {
+        hVzRef3[i] = new TH2F(
+            Form("hVzRef3_%d", cent_conf::trgList[i]), 
+            ";RefMult3;v_{z} [cm]", 
+            850, -0.5, 849.5, 
+            241, -120.5, 120.5
+        );
+    }
+
+    for (Int_t i=0; i<nev; i++) {
+        maker->make(i);
+        int trg = maker->get_converted_trg();
+        if (trg < 0 || trg >= nTrg) {
+            continue;
+        }
+        int vzbin = corr->GetPileUpVzBin(maker->vz);
+        if (vzbin < 0) { continue; }
+        int ref3 = corr->GetRefMult3Corr(maker->RefMult, maker->RefMult3, maker->nTofMatch, maker->nTofBeta, 0, maker->vz, maker->trgid);
+        if (ref3 < 0) {
+            continue;
+        }
+        hVzRef3[trg]->Fill(ref3, maker->vz);
+    }
+
+
+    cout << "[LOG] All done, now saving.\n";
+    TFile* tfout = new TFile("vz_ref3_raw.root", "recreate");
+    tfout->cd();
+    for (int i=0; i<nTrg; i++) {
+        hVzRef3[i]->Write();
+    }
+    tfout->Close();
+    cout << "[LOG] This is the end of this workshop.\n";
+    return 0;
+}
