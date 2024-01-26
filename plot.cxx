01073f9d25ee2526cea842ca729a173661a6b626
Yme <yghuang@mails.ccnu.edu.cn>
2024-01-26 17:38:56 +0100

    v5.1 New plot macro

--- plot.cxx
+++ plot.cxx
@@ -9,10 +9,14 @@
 #include "TLatex.h"
 #include "TLegend.h"
 
+#include <vector>
+#include <algorithm>
+
 int main(int argc, char** argv){
     const char* trg = argv[1];
-    TFile* tf1 = new TFile(Form("high_end_point_vz_%s_raw.root", trg));
-    TFile* tf2 = new TFile(Form("high_end_point_vz_%s_corr.root", trg));
+    const char* ref3Tag = argv[2];
+    TFile* tf1 = new TFile(Form("high_end_point_vz_Ref%s_%s_raw.root", ref3Tag, trg));
+    TFile* tf2 = new TFile(Form("high_end_point_vz_Ref%s_%s_corr.root", ref3Tag, trg));
 
     TGraphErrors* tg1;
     TGraphErrors* tg2;
@@ -25,10 +29,30 @@ int main(int argc, char** argv){
     tf1->GetObject("hfunc", tfunc1);
     tf2->GetObject("hfunc", tfunc2);
 
+    // Axis range finding Block: 
+    int nPoints = tg1->GetN(); // tg1 and tg2 should have the same number of points
+    auto tg1vec = tg1->GetY();
+    auto tg2vec = tg2->GetY();
+    std::vector<Double_t> mVec(tg1vec, tg1vec+nPoints);
+    mVec.insert(mVec.end(), tg2vec, tg2vec+nPoints);
+    double yMax = *std::max_element(mVec.begin(), mVec.end());
+    double yMin = *std::min_element(mVec.begin(), mVec.end());
+    double yMean = yMax*0.5 + yMin*0.5;
+    double yWidth = yMax - yMin;
+    const double factor = 4.5;
+    double yTop = yMean+0.5*factor*yWidth;
+    double yBot = yMean-0.5*factor*yWidth;
+    // std::cout << "DEBUG: ymax " << yMax << std::endl;
+    // std::cout << "DEBUG: ymin " << yMin << std::endl;
+    // std::cout << "DEBUG: ymean " << yMean << std::endl;
+    // std::cout << "DEBUG: ywidth " << yWidth << std::endl;
+    // std::cout << "DEBUG: ytop " << yTop << std::endl;
+    // std::cout << "DEBUG: ybot " << yBot << std::endl;
+
     TCanvas* c = new TCanvas();
     c->cd();
-    TH1* th = gPad->DrawFrame(-75, 550, 75, 640);
-    th->SetTitle(Form("Trigger ID: %s;Vz [cm];High end point", trg));
+    TH1* th = gPad->DrawFrame(-75, yBot, 75, yTop);
+    th->SetTitle(Form("Trigger ID: %s (RefMult%s);Vz [cm];High end point", trg, ref3Tag));
     
     tg1->SetLineColor(1);
     tg1->SetMarkerColor(1);
@@ -57,17 +81,17 @@ int main(int argc, char** argv){
     for (int i=0; i<7; i++){
         if (i==0) {
             // p0 (const) using 2 digits float
-            lat->DrawLatex(-4, 635-i*4, Form("p_{%d}=%6.2f\t\t#Rightarrow%6.2f", i, tfunc1->GetParameter(i), tfunc2->GetParameter(i)));
+            lat->DrawLatexNDC(0.48, 0.85-i*0.03, Form("p_{%d}=%6.2f\t\t#Rightarrow%6.2f", i, tfunc1->GetParameter(i), tfunc2->GetParameter(i)));
         } else {
             // higher orders using 2 scientific form
-            lat->DrawLatex(-4, 635-i*4, Form("p_{%d}=%6.2e\t\t#Rightarrow%6.2e", i, tfunc1->GetParameter(i), tfunc2->GetParameter(i)));
+            lat->DrawLatexNDC(0.48, 0.85-i*0.03, Form("p_{%d}=%6.2e\t\t#Rightarrow%6.2e", i, tfunc1->GetParameter(i), tfunc2->GetParameter(i)));
         }
     }
     lat->SetTextSize(0.05);
-    lat->DrawLatex(0, 570, "h(V_{z})=#sum_{i=0}^{6}p_{i}V_{z}^{i}");
-    lat->DrawLatex(-60, 573, "STAR BES-II");
-    lat->DrawLatex(-60, 558, "Au+Au @ 17.3 GeV");
+    lat->DrawLatexNDC(0.5, 0.30, "h(V_{z})=#sum_{i=0}^{6}p_{i}V_{z}^{i}");
+    lat->DrawLatexNDC(0.24, 0.25, "STAR BES-II");
+    lat->DrawLatexNDC(0.24, 0.20, "Au+Au @ 14.6 GeV");
 
-    c->Print(Form("hvz_%s.pdf", trg));
+    c->Print(Form("hvz_Ref%s_%s.pdf", ref3Tag, trg));
     return 0;
 }
